c494ef969825b578659303cd664a9b9449b97999
Chris Loveless <chriskloveless@gmail.com>
2022-11-14 13:38:42 -0800

    WIP: fixing problem with web dev server

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GalacticAge from './js/galactic-age';
 
-let galacticAge = new GalacticAge(0);
+let galacticAge = new GalacticAge(0,0);
 
 
 function handleSubmit(event) {
   event.preventDefault();
 
   const userAge = document.getElementById("userAge").value;
+  const userExp = document.getElementById("userExp").value;
   galacticAge.age = userAge;
+  galacticAge.lifeExpectancy = userExp;
 
 }
 
