c2048558d67265d684a5edb34493e20a495edd7f
Chris Loveless <chriskloveless@gmail.com>
2022-11-14 15:30:30 -0800

    add ui function to get value of checkbox and display a message

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GalacticAge from './js/galactic-age';
 
-let galacticAge = new GalacticAge(0,0);
+let galacticAge = new GalacticAge(0);
 
 
 function handleSubmit(event) {
   event.preventDefault();
-
-  const userAge = document.getElementById("userAge").value;
-  galacticAge.age = userAge;
   
+  let planetChecked;
+  const planet = document.querySelector("input[type='checkbox']:checked");
+  const userAge = parseInt(document.getElementById("userAge").value);
+  planetChecked = planet.value;
+  galacticAge.age = userAge;
+
+  const mercuryAge = galacticAge.mercury();
+  // const venusAge = galacticAge.venus();
+  document.getElementById("result").innerHTML = `Your age in ${planetChecked} years is ${mercuryAge}`;
 
 }
 
